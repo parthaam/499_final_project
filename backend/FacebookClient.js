fce2eb57720c5d7e43689bd50e4c6bdd08b638f5
parthaam <partha.mahajani@appdirect.com>
2014-04-08 23:18:30 -0700

    Update FacebookClient.js

--- backend/FacebookClient.js
+++ backend/FacebookClient.js
@@ -9,23 +9,23 @@ module.exports = {
        		if (result) {
        			res.send(JSON.parse(result));
        		}
-   		 });
+   	});
     	var options = {
     		host : 'graph.facebook.com',
     		path : '/partha.mahajani',
     		method : 'GET'
   		}
   		var request = http.request(options, function(response) {
-    		var body = ""
-		    response.on('data', function(data) {
-		    	body += data;
-		    });
-		    response.on('end', function() {
-		    	memory_cache.set('partha.mahajani', body, function(err) {
-    				if (err) { throw err; }
-    				res.send(JSON.parse(body));
-    			});
-		    });
+    			var body = "";
+		    	response.on('data', function(data) {
+		    		body += data;
+		    	});
+		    	response.on('end', function() {
+		    		memory_cache.set('partha.mahajani', body, function(err) {
+    					if (err) { throw err; }
+    					res.send(JSON.parse(body));
+    				});
+		    	});
 		});
 		request.on('error', function(e) {
 			console.log('Problem with request: ' + e.message);
